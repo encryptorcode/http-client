a1ed26fa929f660d43be21a228153c4280f204a2
Abhay Jatin Doshi <ajd1996@hotmail.com>
2019-04-18 18:09:59 +0530

    Support for calling APIs based on REST standards 1. HTTPConnector.request to make requests using the HTTPRequest Object to get back a HTTPResponse object 2. Construction of HTTPRequest object using "Fluent Interface" which makes construction of requests and calling the requests easy for the developer 3. HTTPResponse Object that decodes the InputStream and serves you as a String response.
    - This is a wrapper over the OkHttp library. I'd like to change this to wrap over the native Java.net.HttpURLConnection in the coming future
    
    Fluent Interface - https://en.wikipedia.org/wiki/Fluent_interface#Java

---
+++ src/main/java/com/encryptorcode/httpclient/HTTPResponse.java
@@ -0,0 +1,31 @@
+package com.encryptorcode.httpclient;
+
+import okhttp3.ResponseBody;
+
+import java.io.IOException;
+
+public class HTTPResponse {
+    private int responseCode;
+    private String responseMessage;
+    private String body;
+
+    HTTPResponse(int responseCode, String responseMessage, ResponseBody body) throws IOException {
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+        if(body != null) {
+            this.body = body.string();
+        }
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public String getData(){
+        return this.body;
+    }
+}
